8818473ec8b49e87ff6930ec268bd5afc83a7748
Thonatos.Yang <thonatos.yang@gmail.com>
2015-07-05 02:49:02 +0800

    Mod: compile js

--- public/js-spm/dist/MT.SPM/0.1.0/src/page/index-index-debug.js
+++ public/js-spm/dist/MT.SPM/0.1.0/src/page/index-index-debug.js
@@ -337,73 +337,17 @@ var Wechat = {
 
             wx.ready(function(){
 
-                var shareData = {
-                    title: '微信JS-SDK Demo',
-                    desc: '微信JS-SDK,帮助第三方为用户提供更优质的移动web服务',
-                    link: 'http://demo.open.weixin.qq.com/jssdk/',
-                    imgUrl: 'http://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRt8Qia4lv7k3M9J1SKqKCImxJCt7j9rHYicKDI45jRPBxdzdyREWnk0ia0N5TMnMfth7SdxtzMvVgXg/0'
-                };
-                wx.onMenuShareAppMessage(shareData);
-                wx.onMenuShareTimeline(shareData);
+                var $img = '<div style="display: none;"><img src="http://www.thonatos.com/public/images/other/kira.png">';
+                $('body').prepend($img);
 
-
-                var imgUrl = 'http://topic.xcar.com.cn/201403/ad_q3/pic/banner.jpg';
-                var lineLink = 'http://topic.xcar.com.cn/201403/ad_q3/index.php';
-                var descContent = "http://topic.xcar.com.cn/201403/ad_q3/index.php";
-                var shareTitle = '【奥迪Q3开启尊享礼遇季】报名试驾，赢取精美礼品';
-                var appid = 'wxc9937e3a66af6dc8';
-
-                function shareFriend() {
-                    WeixinJSBridge.invoke('sendAppMessage',{
-                        "appid": appid,
-                        "img_url": imgUrl,
-                        "img_width": "640",
-                        "img_height": "640",
-                        "link": lineLink,
-                        "desc": descContent,
-                        "title": shareTitle
-                    }, function(res) {
-                        _report('send_msg', res.err_msg);
-                    })
-                }
-                function shareTimeline() {
-                    WeixinJSBridge.invoke('shareTimeline',{
-                        "img_url": imgUrl,
-                        "img_width": "640",
-                        "img_height": "640",
-                        "link": lineLink,
-                        "desc": descContent,
-                        "title": shareTitle
-                    }, function(res) {
-                        _report('timeline', res.err_msg);
-                    });
-                }
-                function shareWeibo() {
-                    WeixinJSBridge.invoke('shareWeibo',{
-                        "content": descContent,
-                        "url": lineLink,
-                    }, function(res) {
-                        _report('weibo', res.err_msg);
-                    });
-                }
-// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
-                document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-
-                    // 发送给好友
-                    WeixinJSBridge.on('menu:share:appmessage', function(argv){
-                        shareFriend();
-                    });
-
-                    // 分享到朋友圈
-                    WeixinJSBridge.on('menu:share:timeline', function(argv){
-                        shareTimeline();
-                    });
-
-                    // 分享到微博
-                    WeixinJSBridge.on('menu:share:weibo', function(argv){
-                        shareWeibo();
-                    });
-                }, false);
+                //var shareData = {
+                //    title: '微信JS-SDK Demo',
+                //    desc: '微信JS-SDK,帮助第三方为用户提供更优质的移动web服务',
+                //    link: 'http://demo.open.weixin.qq.com/jssdk/',
+                //    imgUrl: 'http://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRt8Qia4lv7k3M9J1SKqKCImxJCt7j9rHYicKDI45jRPBxdzdyREWnk0ia0N5TMnMfth7SdxtzMvVgXg/0'
+                //};
+                //wx.onMenuShareAppMessage(shareData);
+                //wx.onMenuShareTimeline(shareData);
             });
 
 
